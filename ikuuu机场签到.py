dea2d5e30538bbd1da8cc35705799ba35d627e38
指定王子 <30521608+ZhiDingWangZi@users.noreply.github.com>
2024-02-13 01:43:55 +0800

    Update ikuuu机场签到.py
    修改推送为Bark

--- ikuuu机场签到.py
+++ ikuuu机场签到.py
@@ -1,10 +1,3 @@
-# @Source：https://github.com/bighammer-link/jichang_dailycheckin
-# -------------------------------
-# cron "30 4 * * *" script-path=xxx.py,tag=匹配cron用
-# const $ = new Env('ikuuu机场签到')
-# 注册地址：https://ikuuu.me/auth/register?code=lzQE
-
-
 import requests, json, re, os
 
 session = requests.session()
@@ -12,8 +5,6 @@ session = requests.session()
 email = os.environ.get('ikuuuEMAIL')
 # 配置用户名对应的密码 和上面的email对应上
 passwd = os.environ.get('ikuuuPASSWD')
-# server酱
-SCKEY = os.environ.get('SCKEY')
 
 login_url = 'https://ikuuu.me/auth/login'
 check_url = 'https://ikuuu.me/user/checkin'
@@ -33,20 +24,16 @@ try:
     print(response['msg'])
     # 获取账号名称
     info_html = session.get(url=info_url,headers=header).text
-#     info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
-#     print(info)
     # 进行签到
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
     content = result['msg']
     # 进行推送
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuuu自动签到任务提示&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
-        print('推送成功')
+    push_url = f'https://api.day.app/{os.environ.get("BARK_PUSH")}/iKuuu自动签到/{content}'
+    requests.post(url=push_url)
+    print('推送成功')
 except:
     content = '签到失败'
     print(content)
-    if SCKEY != '':
-        push_url = 'https://sctapi.ftqq.com/{}.send?title=ikuuu自动签到任务提示&desp={}'.format(SCKEY, content)
-        requests.post(url=push_url)
+    push_url = f'https://api.day.app/{os.environ.get("BARK_PUSH")}/iKuuu自动签到/{content}'
+    requests.post(url=push_url)
